26637c75405093a1f8a54ba6c5516cabb6318c20
Prash3464 <prash56688@outlook.com>
2026-05-02 18:46:37 +0530

    add in line 143

--- backend/settings.py
+++ backend/settings.py
@@ -140,6 +140,7 @@ REST_FRAMEWORK = {
 
 }
 
+CSP_CONNECT_SRC = ("'self'", "https://teamtaskmanager-backend.onrender.com", "http://127.0.0.1:8000")
 
 CORS_ALLOWED_ORIGINS = [
     'https://web-production-febcc.up.railway.app',
